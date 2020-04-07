9f39763687b099ef262d66d87029ab9c6a754ef0
Anh Minh <anthonyle9x@gmail.com>
2020-04-07 09:40:55 +0700

    Update database-handler.php

--- php/database-handler.php
+++ php/database-handler.php
@@ -5,8 +5,7 @@ $dbUsername = "root";
 $dbPassword = "admin";
 $dbName = "twin_cities";
 $gmapsAPI = "AIzaSyDHj25MUPt4n2aLHslXKQs4GOHy9wdH5_0";
-$twitterToken = "1142284264316125184-6glFiqGhvehlHzZPcuKPm7OAF0bRFJ";
-$twitterTokenSecret ="VUcN38SVFB9OthpO6rK1VXyUYRJs38SczSyctZuoxMr3P";
+
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
