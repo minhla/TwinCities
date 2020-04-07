de8038be383bddcdd0e592bc81cf522cbf4ddcbc
Anh Minh <anthonyle9x@gmail.com>
2020-04-07 09:40:37 +0700

    Create database-handler.php

---
+++ php/database-handler.php
@@ -0,0 +1,13 @@
+<?php
+//This file holds the login information to the database and API keys
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "admin";
+$dbName = "twin_cities";
+$gmapsAPI = "AIzaSyDHj25MUPt4n2aLHslXKQs4GOHy9wdH5_0";
+$twitterToken = "1142284264316125184-6glFiqGhvehlHzZPcuKPm7OAF0bRFJ";
+$twitterTokenSecret ="VUcN38SVFB9OthpO6rK1VXyUYRJs38SczSyctZuoxMr3P";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
